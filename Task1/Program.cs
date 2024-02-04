dd677adb25809a3e71c9f830225a2da725326438
Dmitriu Ydin <da-ekb@mail.ru>
2024-02-05 00:16:20 +0500

    add Les5_final

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿// Условия задачи №1:
+// Вычислить факториал от натурального числа N.
+
+int Fact(int n)
+{
+    if (n == 1 || n == 0)
+    {
+        return 1;
+    }
+    return n * Fact(n - 1);
+}
+
+Console.WriteLine(Fact(5));
